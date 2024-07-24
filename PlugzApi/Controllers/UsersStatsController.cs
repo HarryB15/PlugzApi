763fa3b665dc5d03f6df6f2c2cc705988f25715b
harry <h.birchall@icloud.com>
2024-07-24 13:56:10 +0100

    Added user stats

---
+++ PlugzApi/Controllers/UsersStatsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph.Models;
+using PlugzApi.Models;
+
+namespace PlugzApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersStatsController : ControllerBase
+    {
+        [HttpGet("{userId:int}")]
+        public async Task<ActionResult> GetUsersStats(int userId)
+        {
+            UsersStats stats = new UsersStats();
+            stats.userId = userId;
+            await stats.GetUsersStats();
+            return (stats.error == null) ? Ok(stats) : StatusCode(stats.error.errorCode, stats.error);
+        }
+    }
+}
+
---
+++ PlugzApi/Models/UsersStats.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using PlugzApi.Services;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PlugzApi.Models
+{
+	public class UsersStats: Base
+	{
+		public int totalPurchases { get; set; }
+        public int livePurchases { get; set; }
+        public int awaitingAccPurchases { get; set; }
+        public int totalSales { get; set; }
+        public int liveSales { get; set; }
+        public int awaitingAccSales { get; set; }
+        public UserRatings rating { get; set; } = new UserRatings();
+		public async Task GetUsersStats()
+		{
+
+            try
+            {
+                con = await CommonService.Instance.Open();
+                cmd = new SqlCommand("GetUsersStats", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@userId", SqlDbType.Int).Value = userId;
+                sdr = await cmd.ExecuteReaderAsync();
+                if (sdr.Read())
+                {
+                    totalPurchases = (int)sdr["TotalPurchases"];
+                    livePurchases = (int)sdr["LivePurchases"];
+                    awaitingAccPurchases = (int)sdr["AwaitingAccPurchases"];
+                    totalSales = (int)sdr["TotalSales"];
+                    liveSales = (int)sdr["LiveSales"];
+                    awaitingAccSales = (int)sdr["AwaitingAccSales"];
+                    rating.avgRating = (decimal)sdr["avgRating"];
+                }
+            }
+            catch (Exception ex)
+            {
+                CommonService.Instance.Log(ex);
+                error = CommonService.Instance.GetUnexpectedErrrorMsg();
+            }
+            await CommonService.Instance.Close(con, sdr);
+        }
+	}
+}
+
