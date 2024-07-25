975f875be4a4fc4645e7b1ba791d6f370006889f
harry <h.birchall@icloud.com>
2024-07-25 18:09:31 +0100

    Added posts

---
+++ PlugzApi/Controllers/PostsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PlugzApi.Models;
+
+namespace PlugzApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController: ControllerBase
+	{
+        [HttpPost]
+        public async Task<ActionResult> InsPosts(Posts post)
+        {
+            await post.InsPosts();
+            return (post.error == null) ? Ok() : StatusCode(post.error.errorCode, post.error);
+        }
+        [HttpDelete]
+        public async Task<ActionResult> DeletePost(Posts post)
+        {
+            await post.DeletePost();
+            return (post.error == null) ? Ok() : StatusCode(post.error.errorCode, post.error);
+        }
+        [HttpGet("{userId:int}")]
+        public async Task<ActionResult> GetUsersPosts(int userId)
+        {
+            Posts post = new Posts();
+            post.userId = userId;
+            var posts = await post.GetUsersPosts();
+            return (post.error == null) ? Ok(posts) : StatusCode(post.error.errorCode, post.error);
+        }
+    }
+}
+
---
+++ PlugzApi/Models/Posts.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using PlugzApi.Services;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PlugzApi.Models
+{
+	public class Posts: Base
+	{
+        public int postId { get; set; }
+        public string postText { get; set; } = "";
+        public decimal price { get; set; }
+        public byte minUserRatings { get; set; }
+        public short? minSales { get; set; }
+        public bool isPublic { get; set; }
+        public DateTime createdDatetime { get; set; }
+        public DateTime expiryDatetime { get; set; }
+        public int expiryHours { get; set; }
+        public string userName { get; set; } = "";
+        public bool isConnected { get; set; }
+
+        public async Task InsPosts()
+        {
+            try
+            {
+                con = await CommonService.Instance.Open();
+                cmd = new SqlCommand("InsPosts", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@userId", SqlDbType.Int).Value = userId;
+                cmd.Parameters.Add("@postText", SqlDbType.NVarChar).Value = postText;
+                cmd.Parameters.Add("@price", SqlDbType.Decimal).Value = price;
+                cmd.Parameters.Add("@minUserRatings", SqlDbType.TinyInt).Value = minUserRatings;
+                cmd.Parameters.Add("@minSales", SqlDbType.SmallInt).Value = minSales;
+                cmd.Parameters.Add("@isPublic", SqlDbType.Bit).Value = isPublic;
+                cmd.Parameters.Add("@expiryHours", SqlDbType.Int).Value = expiryHours;
+                await cmd.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                CommonService.Instance.Log(ex);
+                error = CommonService.Instance.GetUnexpectedErrrorMsg();
+            }
+            await CommonService.Instance.Close(con, sdr);
+        }
+        public async Task DeletePost()
+        {
+            try
+            {
+                con = await CommonService.Instance.Open();
+                cmd = new SqlCommand("DeletePost", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@postId", SqlDbType.Int).Value = postId;
+                await cmd.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                CommonService.Instance.Log(ex);
+                error = CommonService.Instance.GetUnexpectedErrrorMsg();
+            }
+        }
+        public async Task<List<Posts>> GetUsersPosts()
+        {
+            List<Posts> posts = new List<Posts>();
+            try
+            {
+                con = await CommonService.Instance.Open();
+                cmd = new SqlCommand("GetUsersPosts", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@userId", SqlDbType.Int).Value = userId;
+                sdr = await cmd.ExecuteReaderAsync();
+                while (sdr.Read())
+                {
+                    Posts post = new Posts()
+                    {
+                        postId = (int)sdr["PostId"],
+                        postText = (string)sdr["PostText"],
+                        price = (decimal)sdr["Price"],
+                        minUserRatings = (byte)sdr["MinUserRatings"],
+                        minSales = (short)sdr["MinSales"],
+                        isPublic = (bool)sdr["IsPublic"],
+                        createdDatetime = (DateTime)sdr["CreatedDatetime"],
+                        expiryDatetime = (DateTime)sdr["ExpiryDatetime"],
+                    };
+                    posts.Add(post);
+                }
+            }
+            catch (Exception ex)
+            {
+                CommonService.Instance.Log(ex);
+                error = CommonService.Instance.GetUnexpectedErrrorMsg();
+            }
+            await CommonService.Instance.Close(con, sdr);
+            return posts;
+        }
+    }
+}
+
