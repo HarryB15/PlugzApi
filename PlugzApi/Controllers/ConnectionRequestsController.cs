039fdc1ff0fd53fa9a4dd0df02e43eb4fa620bfa
harry <h.birchall@icloud.com>
2024-07-27 09:52:19 +0100

    Added connection requests

---
+++ PlugzApi/Controllers/ConnectionRequestsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PlugzApi.Models;
+
+namespace PlugzApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConnectionRequestsController : ControllerBase
+	{
+        [HttpPost]
+        public async Task<ActionResult> InsConnectionRequests(ConnectionRequests request)
+        {
+            await request.InsConnectionRequests();
+            return (request.error == null) ? Ok() : StatusCode(request.error.errorCode, request.error);
+        }
+        [HttpPut]
+        public async Task<ActionResult> ConnectionRequestResponse(ConnectionRequests request)
+        {
+            await request.ConnectionRequestResponse();
+            return (request.error == null) ? Ok() : StatusCode(request.error.errorCode, request.error);
+        }
+        [HttpGet("{userId:int}")]
+        public async Task<ActionResult> GetUserConnectionRequests(int userId)
+        {
+            ConnectionRequests request = new ConnectionRequests();
+            request.userId = userId;
+            var requests = await request.GetUserConnectionRequests();
+            return (request.error == null) ? Ok(requests) : StatusCode(request.error.errorCode, request.error);
+        }
+    }
+}
+
---
+++ PlugzApi/Models/ConnectionRequests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.Graph.Models;
+using PlugzApi.Services;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PlugzApi.Models
+{
+	public class ConnectionRequests: Base
+	{
+		public int requestId { get; set; }
+        public int connectionUserId { get; set; }
+        public DateTime requestDatetime { get; set; }
+        public bool accepted { get; set; }
+        public string userName { get; set; } = "";
+
+        public async Task InsConnectionRequests()
+        {
+            try
+            {
+                con = await CommonService.Instance.Open();
+                cmd = new SqlCommand("InsConnectionRequests", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@userId", SqlDbType.Int).Value = userId;
+                cmd.Parameters.Add("@connectionUserId", SqlDbType.Int).Value = connectionUserId;
+                await cmd.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                CommonService.Instance.Log(ex);
+                error = CommonService.Instance.GetUnexpectedErrrorMsg();
+            }
+            await CommonService.Instance.Close(con, sdr);
+        }
+        public async Task ConnectionRequestResponse()
+        {
+            try
+            {
+                con = await CommonService.Instance.Open();
+                cmd = new SqlCommand("ConnectionRequestResponse", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@requestId", SqlDbType.Int).Value = requestId;
+                cmd.Parameters.Add("@accepted", SqlDbType.Bit).Value = accepted;
+                await cmd.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                CommonService.Instance.Log(ex);
+                error = CommonService.Instance.GetUnexpectedErrrorMsg();
+            }
+            await CommonService.Instance.Close(con, sdr);
+        }
+        public async Task<List<ConnectionRequests>> GetUserConnectionRequests()
+        {
+            List<ConnectionRequests> requests = new List<ConnectionRequests>();
+            try
+            {
+                con = await CommonService.Instance.Open();
+                cmd = new SqlCommand("GetUserConnectionRequests", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@userId", SqlDbType.Int).Value = userId;
+                sdr = await cmd.ExecuteReaderAsync();
+                while (sdr.Read())
+                {
+                    ConnectionRequests request = new ConnectionRequests()
+                    {
+                        requestId = (int)sdr["requestId"],
+                        userId = (int)sdr["UserId"],
+                        connectionUserId = userId,
+                        requestDatetime = (DateTime)sdr["RequestDatetime"],
+                        userName = (string)sdr["UserName"]
+                    };
+                    requests.Add(request);
+                }
+            }
+            catch (Exception ex)
+            {
+                CommonService.Instance.Log(ex);
+                error = CommonService.Instance.GetUnexpectedErrrorMsg();
+            }
+            await CommonService.Instance.Close(con, sdr);
+            return requests;
+        }
+    }
+}
