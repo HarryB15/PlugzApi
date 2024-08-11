32ae9bd5854ca50fce42df32072aa0836c1bcc22
harry <h.birchall@icloud.com>
2024-08-11 15:32:32 +0100

    Added stripe webhook

---
+++ PlugzApi/Controllers/StripeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PlugzApi.Services;
+using Stripe;
+
+namespace PlugzApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StripeController: ControllerBase
+	{
+        [HttpPost]
+        public async Task<IActionResult> PaymentComplete()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            try
+            {
+                var stripeEvent = EventUtility.ParseEvent(json);
+                var signatureHeader = Request.Headers["Stripe-Signature"];
+                stripeEvent = EventUtility.ConstructEvent(json, signatureHeader, CommonService.Instance.GetConfig("StripeSK"));
+                StripeService stripeService = new StripeService();
+                if (await stripeService.PaymentComplete(stripeEvent))
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return StatusCode(500);
+                }
+            }
+            catch (StripeException ex)
+            {
+                CommonService.Log(ex);
+                return StatusCode(500);
+            }
+        }
+    }
+}
+
--- PlugzApi/Services/StripeService.cs
+++ PlugzApi/Services/StripeService.cs
@@ -34,6 +34,66 @@ namespace PlugzApi.Services
                 return null;
             }
         }
+        public async Task<bool> PaymentComplete(Event stripeEvent)
+        {
+            bool success = false;
+            try
+            {
+                var paymentIntent = new PaymentIntent();
+                paymentIntent = stripeEvent.Data.Object as PaymentIntent;
+                if (paymentIntent != null)
+                {
+                    success = await HandlePaymentComplete(paymentIntent.Id);
+                    if (!success)
+                    {
+                        await InsPayCompFailures(paymentIntent.Id, paymentIntent.Amount / 100.0m);
+                        SentrySdk.CaptureMessage("Error with payment: " + paymentIntent.Id, SentryLevel.Fatal);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                CommonService.Log(ex);
+            }
+            return success;
+        }
+        private static async Task<bool> HandlePaymentComplete(string payIntentId)
+        {
+            bool success = false;
+            try
+            {
+                var con = await CommonService.Instance.Open();
+                var cmd = new SqlCommand("PaymentCompleted", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@payIntentId", SqlDbType.VarChar).Value = payIntentId;
+                await cmd.ExecuteNonQueryAsync();
+                success = true;
+            }
+            catch (Exception ex)
+            {
+                CommonService.Log(ex);
+            }
+            return success;
+        }
+        private static async Task<bool> InsPayCompFailures(string payIntentId, decimal amount)
+        {
+            bool success = false;
+            try
+            {
+                var con = await CommonService.Instance.Open();
+                var cmd = new SqlCommand("InsPayCompFailures", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@payIntentId", SqlDbType.VarChar).Value = payIntentId;
+                cmd.Parameters.Add("@amount", SqlDbType.Decimal).Value = amount;
+                await cmd.ExecuteNonQueryAsync();
+                success = true;
+            }
+            catch (Exception ex)
+            {
+                CommonService.Log(ex);
+            }
+            return success;
+        }
     }
 }
 
