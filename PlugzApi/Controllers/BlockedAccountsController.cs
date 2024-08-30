3eb6659e3ec273ec50c30ee7028f34523e612cf2
harry <h.birchall@icloud.com>
2024-08-30 20:51:40 +0100

    Added blocked accounts

---
+++ PlugzApi/Controllers/BlockedAccountsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PlugzApi.Models;
+
+namespace PlugzApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlockedAccountsController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> InsBlockedAccounts(BlockedAccounts blockedAccount)
+        {
+            await blockedAccount.InsBlockedAccounts();
+            return (blockedAccount.error == null) ? Ok() : StatusCode(blockedAccount.error.errorCode, blockedAccount.error);
+        }
+        [HttpDelete]
+        public async Task<ActionResult> DeleteBlockedAccount(BlockedAccounts blockedAccount)
+        {
+            await blockedAccount.DeleteBlockedAccount();
+            return (blockedAccount.error == null) ? Ok() : StatusCode(blockedAccount.error.errorCode, blockedAccount.error);
+        }
+        [HttpGet("{userId:int}")]
+        public async Task<ActionResult> GetUsersBlockedAccounts(int userId)
+        {
+            BlockedAccounts blockedAccount = new BlockedAccounts();
+            blockedAccount.userId = userId;
+            var blockedAccounts = await blockedAccount.GetUsersBlockedAccounts();
+            return (blockedAccount.error == null) ? Ok(blockedAccounts) : StatusCode(blockedAccount.error.errorCode, blockedAccount.error);
+        }
+    }
+}
+
---
+++ PlugzApi/Models/BlockedAccounts.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using PlugzApi.Services;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PlugzApi.Models
+{
+	public class BlockedAccounts: Base
+	{
+        public int blockedId { get; set; }
+        public int blockedUserId { get; set; }
+        public string userName { get; set; } = "";
+
+        public async Task InsBlockedAccounts()
+        {
+            try
+            {
+                con = await CommonService.Instance.Open();
+                cmd = new SqlCommand("InsBlockedAccounts", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@userId", SqlDbType.Int).Value = userId;
+                cmd.Parameters.Add("@blockedUserId", SqlDbType.Int).Value = blockedUserId;
+                await cmd.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                CommonService.Log(ex);
+                error = CommonService.GetUnexpectedErrrorMsg();
+            }
+            await CommonService.Close(con, sdr);
+        }
+        public async Task DeleteBlockedAccount()
+        {
+            try
+            {
+                con = await CommonService.Instance.Open();
+                cmd = new SqlCommand("DeleteBlockedAccount", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@blockedId", SqlDbType.Int).Value = blockedId;
+                await cmd.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                CommonService.Log(ex);
+                error = CommonService.GetUnexpectedErrrorMsg();
+            }
+            await CommonService.Close(con, sdr);
+        }
+        public async Task<List<BlockedAccounts>> GetUsersBlockedAccounts()
+        {
+            List<BlockedAccounts> blockedAccounts = new List<BlockedAccounts>();
+            try
+            {
+                con = await CommonService.Instance.Open();
+                cmd = new SqlCommand("GetUsersBlockedAccounts", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@userId", SqlDbType.Int).Value = userId;
+                sdr = await cmd.ExecuteReaderAsync();
+                while (sdr.Read())
+                {
+                    blockedAccounts.Add(new BlockedAccounts()
+                    {
+                        blockedId = (int)sdr["BlockedId"],
+                        blockedUserId = (int)sdr["BlockedUserId"],
+                        userName = (string)sdr["UserName"]
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                CommonService.Log(ex);
+                error = CommonService.GetUnexpectedErrrorMsg();
+            }
+            await CommonService.Close(con, sdr);
+            return blockedAccounts;
+        }
+    }
+}
+
