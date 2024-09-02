e97cf472af4a4eabe4f8e7b24ab24276aba4701e
harry <h.birchall@icloud.com>
2024-09-02 21:08:34 +0100

    Added pickup drop off to ins offer

--- PlugzApi/Models/Offer.cs
+++ PlugzApi/Models/Offer.cs
@@ -14,6 +14,9 @@ namespace PlugzApi.Models
 		public decimal offerValue { get; set; }
         public string? responseType { get; set; }
         public int? oriOfferId { get; set; }
+        public string pickUpDropOff { get; set; } = "";
+        public string? pickupAddress { get; set; }
+        public Location? pickupLocation { get; set; }
         public Listings listing { get; set; } = new Listings();
         public async Task InsOffer(int receiverUserId)
 		{
@@ -27,6 +30,13 @@ namespace PlugzApi.Models
                 cmd.Parameters.Add("@offerValue", SqlDbType.Decimal).Value = offerValue;
                 cmd.Parameters.Add("@offerText", SqlDbType.NVarChar).Value = offerText;
                 cmd.Parameters.Add("@oriOfferId", SqlDbType.Int).Value = oriOfferId;
+                cmd.Parameters.Add("@pickUpDropOff", SqlDbType.Char).Value = pickUpDropOff;
+                if (pickUpDropOff == "P" && pickupLocation != null)
+                {
+                    cmd.Parameters.Add("@pickupAddress", SqlDbType.VarChar).Value = pickupAddress;
+                    cmd.Parameters.Add("@pickupLat", SqlDbType.Decimal).Value = pickupLocation.lat;
+                    cmd.Parameters.Add("@pickupLng", SqlDbType.Decimal).Value = pickupLocation.lng;
+                }
                 cmd.Parameters.Add("@receiverUserId", SqlDbType.Int).Value = receiverUserId;
                 sdr = await cmd.ExecuteReaderAsync();
                 if (sdr.Read())
