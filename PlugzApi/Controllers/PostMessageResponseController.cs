ebe866e798f71b9665b20d1f95d98e6155a1b86b
harry <h.birchall@icloud.com>
2024-09-15 10:03:49 +0100

    Added post message response

---
+++ PlugzApi/Controllers/PostMessageResponseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PlugzApi.Models;
+
+namespace PlugzApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostMessageResponseController: ControllerBase
+	{
+        [HttpGet]
+        public async Task<ActionResult> GetPostMessageResponses(int postMessageId)
+        {
+            PostMessageResponse response = new PostMessageResponse();
+            response.postMessageId = postMessageId;
+            var responses = await response.GetPostMessageResponses();
+            return (response.error == null) ? Ok(responses) : StatusCode(response.error.errorCode, response.error);
+        }
+        [HttpPost]
+        public async Task<ActionResult> UpsertPostMessageResponses(PostMessageResponse response)
+        {
+            await response.UpsertPostMessageResponses();
+            return (response.error == null) ? Ok() : StatusCode(response.error.errorCode, response.error);
+        }
+    }
+}
+
